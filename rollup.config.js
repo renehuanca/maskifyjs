15c713de734be2a58767280432ae9d6d4b076afa
Rene Huanca <renehuanca999@gmail.com>
2025-02-22 12:00:35 -0400

    feat: new features xd

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,24 @@
-import { terser } from 'rollup-plugin-terser'
+import { terser } from 'rollup-plugin-terser';
+import typescript from '@rollup/plugin-typescript';
 
 export default {
-    input: "src/maskify.js",
+    input: "src/index.ts",
     output: [
-        // to import ESM
+        // Formato ESM
         {
             file: "dist/maskify.mjs",
-            format: 'esm'
+            format: 'esm',
+            plugins: [terser()]  // Minificación para la versión ESM
         },
+        // Formato CommonJS
         {
             file: "dist/maskify.min.js",
             format: 'cjs',
-            name: "maskify"
+            name: "maskify",  // Asegura que CommonJS tenga el nombre correcto
+            plugins: [terser()]  // Minificación para la versión CJS
         }
     ],
-    plugins: [terser()]
-}
+    plugins: [
+        typescript() // Transpila el código TypeScript
+    ]
+};
