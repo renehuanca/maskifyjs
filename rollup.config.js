c7e07149e73fa23c8872028b6969346fd770140a
Rene Huanca <renehuanca999@gmail.com>
2025-02-24 19:24:29 -0400

    feat: add linting script and update code formatting

--- rollup.config.js
+++ rollup.config.js
@@ -1,49 +1,49 @@
-import terser from '@rollup/plugin-terser';
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import postcss from 'rollup-plugin-postcss';
-import cssnano from 'cssnano';
-import copy from 'rollup-plugin-copy';
+import terser from '@rollup/plugin-terser'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import postcss from 'rollup-plugin-postcss'
+import cssnano from 'cssnano'
+import copy from 'rollup-plugin-copy'
 
 export default {
-    input: "src/index.ts",
+  input: 'src/index.ts',
 
-    output: [
-        {
-            file: "dist/maskify.cjs.js",
-            format: 'cjs',
-            plugins: [terser()],
-        },
-        {
-            file: "dist/maskify.esm.js",
-            format: 'esm',
-            plugins: [terser()],
-        },
-        {
-            file: "dist/maskify.min.js",
-            format: 'umd',
-            name: "Maskify",
-            plugins: [terser()]
-        }
-    ],
+  output: [
+    {
+      file: 'dist/maskify.cjs.js',
+      format: 'cjs',
+      plugins: [terser()]
+    },
+    {
+      file: 'dist/maskify.esm.js',
+      format: 'esm',
+      plugins: [terser()]
+    },
+    {
+      file: 'dist/maskify.min.js',
+      format: 'umd',
+      name: 'Maskify',
+      plugins: [terser()]
+    }
+  ],
 
-    plugins: [
-        resolve(),
-        commonjs(),
-        typescript({
-            tsconfig: './tsconfig.json'
-        }),
-        postcss({
-            extract: 'style.min.css',
-            plugins: [cssnano()],
-        }),
-        copy({
-            targets: [
-                { src: 'src/assets/*.svg', dest: 'dist/assets' }
-            ]
-        }),
-    ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig.json'
+    }),
+    postcss({
+      extract: 'style.min.css',
+      plugins: [cssnano()]
+    }),
+    copy({
+      targets: [
+        { src: 'src/assets/*.svg', dest: 'dist/assets' }
+      ]
+    })
+  ],
 
-    external: [],
-};
+  external: []
+}
--- src/index.ts
+++ src/index.ts
@@ -1,91 +1,91 @@
-import './style.css';
+import './style.css'
 
 interface MaskifyConfig {
-    maxSize?: number;
-    minSize?: number;
+  maxSize?: number
+  minSize?: number
 }
 
 const MASKIFY_CONFIG: MaskifyConfig = {
-    maxSize: 320,
-    minSize: 32
-};
-
-class Maskify {
-    private mask: HTMLElement | null;
-    private baseX: number;
-    private baseY: number;
-    private maxSize: number;
-    private minSize: number;
-
-    constructor(element: string, config: MaskifyConfig = {}) {
-        this.mask = document.querySelector<HTMLElement>(element);
-
-        if (!this.mask) {
-            console.error('Mask element not found for the provided selector: ', element);
-        }
-
-        this.mask.style.height = `${Math.max(
-            document.documentElement.scrollHeight,
-            document.body.scrollHeight
-        )}px`;
-
-        this.baseX = 0;
-        this.baseY = 0;
-        this.maxSize = config.maxSize ?? MASKIFY_CONFIG.maxSize;
-        this.minSize = config.minSize ?? MASKIFY_CONFIG.minSize;
-
-        this.setupListeners();
-    }
-
-    private setupListeners(): void {
-        window.addEventListener('mousemove', (e: MouseEvent) => {
-            this.baseX = e.clientX;
-            this.baseY = e.clientY;
-            this.updateMaskPosition();
-        });
-
-        window.addEventListener('scroll', () => {
-            this.updateMaskPosition();
-        });
-
-        document.querySelectorAll('.mask-effect').forEach((element: HTMLElement) => {
-            element.addEventListener('mouseenter', () => this.animateMaskSize(this.minSize, this.maxSize));
-            element.addEventListener('mouseleave', () => this.animateMaskSize(this.maxSize, this.minSize));
-        });
-    }
-
-    private updateMaskPosition(): void {
-        if (this.mask) {
-            const absX = this.baseX + window.scrollX;
-            const absY = this.baseY + window.scrollY;
-            document.documentElement.style.setProperty('--mask-x', `${absX}px`);
-            document.documentElement.style.setProperty('--mask-y', `${absY}px`);
-        }
-    }
-
-    private animateMaskSize(startSize: number, endSize: number): void {
-        const currentSize: number = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('--mask-size')) || startSize;;
-        const duration: number = 500;
-        let start: number = performance.now();
-
-        const animate = (time: number): void => {
-            let progress: number = (time - start) / duration;
-            if (progress > 1) progress = 1;
-
-            let currentMaskSize: number = currentSize + (endSize - currentSize) * progress;
-            document.documentElement.style.setProperty('--mask-size', `${currentMaskSize}px`);
-
-            if (progress < 1) {
-                requestAnimationFrame(animate);
-            }
-        };
-
-        requestAnimationFrame(animate);
-    }
+  maxSize: 320,
+  minSize: 32
 }
 
-if (typeof module !== "undefined" && module.exports) {
-    module.exports = Maskify;
+class Maskify {
+  private readonly mask: HTMLElement | null
+  private baseX: number
+  private baseY: number
+  private readonly maxSize: number
+  private readonly minSize: number
+
+  constructor (element: string, config: MaskifyConfig = {}) {
+    this.mask = document.querySelector<HTMLElement>(element)
+
+    if (!this.mask) {
+      console.error('Mask element not found for the provided selector: ', element)
+    }
+
+    this.mask.style.height = `${Math.max(
+            document.documentElement.scrollHeight,
+            document.body.scrollHeight
+        )}px`
+
+    this.baseX = 0
+    this.baseY = 0
+    this.maxSize = config.maxSize ?? MASKIFY_CONFIG.maxSize
+    this.minSize = config.minSize ?? MASKIFY_CONFIG.minSize
+
+    this.setupListeners()
+  }
+
+  private setupListeners (): void {
+    window.addEventListener('mousemove', (e: MouseEvent) => {
+      this.baseX = e.clientX
+      this.baseY = e.clientY
+      this.updateMaskPosition()
+    })
+
+    window.addEventListener('scroll', () => {
+      this.updateMaskPosition()
+    })
+
+    document.querySelectorAll('.mask-effect').forEach((element: HTMLElement) => {
+      element.addEventListener('mouseenter', () => this.animateMaskSize(this.minSize, this.maxSize))
+      element.addEventListener('mouseleave', () => this.animateMaskSize(this.maxSize, this.minSize))
+    })
+  }
+
+  private updateMaskPosition (): void {
+    if (this.mask) {
+      const absX = this.baseX + window.scrollX
+      const absY = this.baseY + window.scrollY
+      document.documentElement.style.setProperty('--mask-x', `${absX}px`)
+      document.documentElement.style.setProperty('--mask-y', `${absY}px`)
+    }
+  }
+
+  private animateMaskSize (startSize: number, endSize: number): void {
+    const currentSize: number = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('--mask-size')) || startSize
+    const duration: number = 500
+    const start: number = performance.now()
+
+    const animate = (time: number): void => {
+      let progress: number = (time - start) / duration
+      if (progress > 1) progress = 1
+
+      const currentMaskSize: number = currentSize + (endSize - currentSize) * progress
+      document.documentElement.style.setProperty('--mask-size', `${currentMaskSize}px`)
+
+      if (progress < 1) {
+        requestAnimationFrame(animate)
+      }
+    }
+
+    requestAnimationFrame(animate)
+  }
+}
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = Maskify
 } else {
-    (window as any).Maskify = Maskify;
+  (window as any).Maskify = Maskify
 }
